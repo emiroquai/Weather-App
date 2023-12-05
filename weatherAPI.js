47bbf9b7e799656221e47ed9e2cc6d5097b11c64
emiroquai <22440776+emiroquai@users.noreply.github.com.>
2023-12-05 15:39:10 +0100

    Fetch updated to https

--- weatherAPI.js
+++ weatherAPI.js
@@ -3,7 +3,7 @@ const API = (() => {
     async function getData(city) {
         try {
             const key = '79859debb837432f87f100612230112'
-            const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=3&aqi=no&alerts=no`);
+            const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=3&aqi=no&alerts=no`);
             if (!response.ok) throw new Error(`City ${city} not found`);
             const weatherData = await response.json();
             console.log(weatherData)
